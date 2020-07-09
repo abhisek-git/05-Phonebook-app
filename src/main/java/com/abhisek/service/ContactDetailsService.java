c34e0245364a0426c53a0696fd4a20270d4562fa
abhisek-git <abhi.panigrahi93@gmail.com>
2020-07-09 12:18:50 +0530

    done

--- src/main/java/com/abhisek/service/ContactDetailsService.java
+++ src/main/java/com/abhisek/service/ContactDetailsService.java
@@ -2,7 +2,6 @@ package com.abhisek.service;
 
 import java.util.List;
 
-
 import com.abhisek.dto.Contact;
 
 public interface ContactDetailsService {
@@ -11,8 +10,6 @@ public interface ContactDetailsService {
 	List<Contact> getAllContacts();
 
 	Contact getContactById(Integer contactId);
-
-	//boolean updateContact(Contact contact);
 
 	boolean deleteContact(Integer contactId);
 }
--- src/main/java/com/abhisek/service/ContactDetailsServiceImpl.java
+++ src/main/java/com/abhisek/service/ContactDetailsServiceImpl.java
@@ -1,6 +1,5 @@
 package com.abhisek.service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -15,55 +14,44 @@ import com.abhisek.repository.ContactDetailsRepository;
 
 @Service
 public class ContactDetailsServiceImpl implements ContactDetailsService {
-	
+
 	@Autowired
 	private ContactDetailsRepository repository;
 
 	@Override
 	public boolean saveContact(Contact contact) {
-		ContactDetails contactDetails=new ContactDetails();
-		
+		ContactDetails contactDetails = new ContactDetails();
+
 		BeanUtils.copyProperties(contact, contactDetails);
-		ContactDetails savedEntity=repository.save(contactDetails);
-		return savedEntity.getContactId()!=null;
+		ContactDetails savedEntity = repository.save(contactDetails);
+		return savedEntity.getContactId() != null;
 	}
 
 	@Override
 	public List<Contact> getAllContacts() {
-		List<ContactDetails> entities=repository.findAll();
-		/*List<Contact> contacts=new ArrayList<>();
-		//legacy approach
-		for(ContactDetails entity:entities) {
-			Contact contact=new Contact();
+		List<ContactDetails> entities = repository.findAll();
+		return entities.stream().map(entity -> {
+			Contact contact = new Contact();
 			BeanUtils.copyProperties(entity, contact);
-			contacts.add(contact);
-		}
-		return contacts;*/
-		
-		//java8 approach
-	List<Contact> contacts2=entities.stream().map(entity->{
-		Contact contact=new Contact();
-		BeanUtils.copyProperties(entity, contact);
-		return contact;
-	}).collect(Collectors.toList());
-	return contacts2;
+			return contact;
+		}).collect(Collectors.toList());
 	}
 
 	@Override
 	public Contact getContactById(Integer contactId) {
-		Optional<ContactDetails> contactDetails=repository.findById(contactId);
-		if(contactDetails.isPresent()) {
-			ContactDetails c=contactDetails.get();
-			Contact contact=new Contact();
+		Optional<ContactDetails> contactDetails = repository.findById(contactId);
+		if (contactDetails.isPresent()) {
+			ContactDetails c = contactDetails.get();
+			Contact contact = new Contact();
 			BeanUtils.copyProperties(c, contact);
 			return contact;
 		}
 		return null;
 	}
+
 	@Override
 	public boolean deleteContact(Integer contactId) {
 		repository.deleteById(contactId);
-		System.out.println("the record is deleted successfully with id :"+contactId);
 		return true;
 	}
 
